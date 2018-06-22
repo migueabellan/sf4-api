2bbe94572b5592fead562ce77763fdc2a7fcfe0f
Miguel Ángel <migueabellan@gmail.com>
2018-06-22 17:23:53 +0000

    Add routing

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,18 +2,23 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class IndexController extends Controller
 {
     /**
      * @Route("/", name="index")
+     * @param Request $request
+     * @return string
      */
     public function index()
     {
         echo "IndexController"; die;
         return $this->redirectToRoute('api_doc');
     }
+
 }
